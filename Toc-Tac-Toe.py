4241ab689a7f3aea066b383be8025e36569fa5e3
IvanDemin3467 <85740662+IvanDemin3467@users.noreply.github.com>
2021-07-21 12:46:36 +0600

    Comments and input checks
    Code was fully commented. Inputs were filtered to avoid errors. Also display_board() function was improved to be more stylish.

--- Toc-Tac-Toe.py
+++ Toc-Tac-Toe.py
@@ -2,20 +2,29 @@ from random import randrange
 
 def display_board(board):
     # The function accepts one parameter containing the board's current status
-    # and prints it out to the console.
-    print("+-------+-------+-------+")
-    print("|       |       |       |")
-    print("|   " + board[0][0] + "   |   " + board[0][1] + "   |   " + board[0][2] + "   |")
-    print("|       |       |       |")
-    print("+-------+-------+-------+")
-    print("|       |       |       |")
-    print("|   " + board[1][0] + "   |   " + board[1][1] + "   |   " + board[1][2] + "   |")
-    print("|       |       |       |")
-    print("+-------+-------+-------+")
-    print("|       |       |       |")
-    print("|   " + board[2][0] + "   |   " + board[2][1] + "   |   " + board[2][2] + "   |")
-    print("|       |       |       |")
-    print("+-------+-------+-------+")
+    # and prints it out to the console. It's appearance should be:
+    # +-------+-------+-------+
+    # |       |       |       |
+    # |   1   |   2   |   3   |
+    # |       |       |       |
+    # +-------+-------+-------+
+    # |       |       |       |
+    # |   4   |   5   |   6   |
+    # |       |       |       |
+    # +-------+-------+-------+
+    # |       |       |       |
+    # |   7   |   8   |   9   |
+    # |       |       |       |
+    # +-------+-------+-------+
+    for i in range(3):
+        print(("+"+"-"*7)*3+"+")
+        print(("|"+" "*7)*3+"|")
+        for j in range(3):
+            print("|   " + board[i][j] + "   ", end="")
+        print("|")
+        print(("|"+" "*7)*3+"|")
+    print(("+"+"-"*7)*3+"+")
+
 
 
 def make_list_of_free_fields(board):
@@ -32,24 +41,41 @@ def make_list_of_free_fields(board):
 def enter_move(board):
     # The function accepts the board current status, asks the user about their move, 
     # checks the input and updates the board according to the user's decision.
-    move = int(input("Enter your move: ")) - 1
-    list_of_free_fields = make_list_of_free_fields(board)
+    # Uses recursion to restart on invalid input.
+    # Questionable solution as the user might try to overflow memory.
+    # Recursion is only used for demonstration
+    try:                                   # User makes a move
+        move = int(input("Enter your move: ")) - 1 
+    except ValueError:                     # If user inputs not integer -> try again
+        print("Only integers in range (1 .. 9) accepted. Try again.")
+        enter_move(board)                  # Recursion to start over
+        return
+    if move not in range(9):               # If incorrect input -> try again
+        print("Out of range. Use integers in range (1 .. 9). Try again.")
+        enter_move(board)                  # Recursion to start over
+        return
+    # Converting user's move to coordinates:
     move_row = move // 3
     move_col = move % 3
     move_tup = (move_row, move_col)
-    if move_tup in list_of_free_fields:
-        board[move_row][move_col] = "O"
-    else:
-        print("Already taken")
-        enter_move(board)
+    # Checking if the field is empty
+    list_of_free_fields = make_list_of_free_fields(board)
+    if move_tup in list_of_free_fields:    # Yes, the field is empty
+        board[move_row][move_col] = "O"    # Occupy that field
+    else:                                  # No, the field is occupied
+        print("Already taken. Try again.") 
+        enter_move(board)                  # Recursion to start over
+        return
 
 
 def draw_move(board):
     # The function draws the computer's move and updates the board.
-    list_of_free_fields = make_list_of_free_fields(board)
-    move = randrange(len(list_of_free_fields))
+    list_of_free_fields = make_list_of_free_fields(board) 
+    move = randrange(len(list_of_free_fields)) # Computer plays randomly
+    # Getting coords of chosen field
     move_row = list_of_free_fields[move][0]
     move_col = list_of_free_fields[move][1]
+    # Occupy that field
     board[move_row][move_col] = "X"
     print("My move: ", move_row * 3 + move_col + 1)
 
@@ -57,31 +83,43 @@ def draw_move(board):
 def victory_for(board, sign):
     # The function analyzes the board status in order to check if 
     # the player using 'O's or 'X's has won the game
-    for i in range(3): #checks rows and cols
+
+    # Checks rows and cols
+    for i in range(3): 
         if (board[i][0] == sign and board[i][1] == sign and board[i][2] == sign) or \
            (board[0][i] == sign and board[1][i] == sign and board[2][i] == sign):
             return True
+    # Checks diagonals
     if (board[0][0] == sign and board[1][1] == sign and board[2][2] == sign) or \
        (board[0][2] == sign and board[1][1] == sign and board[2][0] == sign):
         return True
+    # No lines found -> no winner
     return False
 
 
+#####################################################################################
+
+# Init the board
 board = [[str(i+j) for j in range(3)] for i in range(1,9,3)]
 board[1][1] = "X"
 display_board(board)
-
+# Starting game in infinite loop
 while True:
+    # User's turn
     enter_move(board)
     display_board(board)
-    if victory_for(board, "O"):
+    # Checks if user won
+    if victory_for(board, "O"):                   
         print("You won!")
         break
+    # Computer's turn
     draw_move(board)
     display_board(board)
-    if victory_for(board, "X"):
+    # Checks if computer won
+    if victory_for(board, "X"):                   
         print("Computer won!")
         break
-    if len(make_list_of_free_fields(board)) == 0:
+    # Checks if board is full -> no winner
+    if len(make_list_of_free_fields(board)) == 0: 
         print("Tie!")
         break
